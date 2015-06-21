19355d0e61091378462cb3f09440f353d2aec0f6
https://github.com/KentaSugaoi/RouletteApp <kenta-resign@live.jp>
2015-06-21 18:17:21 +0900

    コメントを追加

--- RouletteApp/src/com/example/rouletteapp/MainActivity.java
+++ RouletteApp/src/com/example/rouletteapp/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends ActionBarActivity {
 		//開始ボタン
 		if (id == R.id.startButton){
 			// kaii kaii update yamayama
+			// sugaoi push
 			//view.startRotateCircle();
 		}
 
