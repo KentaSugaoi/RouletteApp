9e70f502fe40f4c77d49c675721ea4e114a1829f
TY <TY@TY-PC>
2015-06-21 18:11:48 +0900

    aaa

--- RouletteApp/src/com/example/rouletteapp/MainActivity.java
+++ RouletteApp/src/com/example/rouletteapp/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
 
 		//開始ボタン
 		if (id == R.id.startButton){
-			// kaii kaii update
+			// kaii kaii update yamayama
 			//view.startRotateCircle();
 		}
 
