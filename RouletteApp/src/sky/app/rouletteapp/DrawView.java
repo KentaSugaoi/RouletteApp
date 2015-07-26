dfca7fbafa0f6d34bec5e2e5d8c6e8c19a173857
https://github.com/KentaSugaoi/RouletteApp <kenta-resign@live.jp>
2015-07-26 15:17:51 +0900

    パッケージ名を変更

--- RouletteApp/src/sky/app/rouletteapp/DrawView.java
+++ RouletteApp/src/sky/app/rouletteapp/DrawView.java
@@ -1,4 +1,4 @@
-package com.example.rouletteapp;
+package sky.app.rouletteapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
--- RouletteApp/src/sky/app/rouletteapp/MainActivity.java
+++ RouletteApp/src/sky/app/rouletteapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.rouletteapp;
+package sky.app.rouletteapp;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
